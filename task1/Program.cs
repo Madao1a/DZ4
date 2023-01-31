27169210306a5edead17b4c666beae695b5721c7
MARS <leprcoin@gmail.com>
2023-01-31 11:01:39 +0400

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] numbers = new int[8];
+Random rand = new Random();
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i]= rand.Next(100,999);
+    Console.WriteLine (numbers[i]);
+}
+int count=0;
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (numbers[j]%2==0)
+    {
+        count++;
+    }
+}
+Console.WriteLine (count);
