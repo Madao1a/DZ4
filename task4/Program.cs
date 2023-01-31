ac8c6cd9a3392559e274eb25283681984a0214c7
MARS <leprcoin@gmail.com>
2023-01-31 13:09:13 +0400

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Дан список некоторых целых чисел, найдите значение 20 в нем и, если оно присутствует, замените его на 200. Обновите список только при первом вхождении числа 20
+int[] numbers = new int[15];
+Random rand = new Random();
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = rand.Next(18,22);
+    Console.Write (numbers[i]);
+}
+int per = 0;
+for (int j = 0; j < numbers.Length && per == 0; j++)
+{
+    if (numbers[j] == 20)
+    {
+        numbers[j] = numbers[j]*10;
+        per = 1;
+    }
+}
+
+Console.WriteLine();
+
+for (int f = 0; f < numbers.Length; f++)
+{
+    Console.Write (numbers[f ]);
+}
+
