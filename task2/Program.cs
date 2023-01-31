ef74d2e6dbe710793b665a827601ae419037a5a7
MARS <leprcoin@gmail.com>
2023-01-31 11:15:14 +0400

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] numbers = new int[8];
+Random rand = new Random();
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = rand.Next(1,10);
+}
+int sum =0;
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (j%2!=0)
+    {
+        sum = sum+numbers[j];
+    }
+}
+Console.WriteLine (sum);
